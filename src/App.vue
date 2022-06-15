4342730a6bb91e1cea574eae4c3f855c93e761d3
ren40 <renat.1998.ru@gmail.com>
2022-06-15 23:22:48 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import vcheckbox from "./components/VCheckbox.vue";
+
+import { ref } from "vue";
+
+const idCheckBox = ref("test1");
+</script>
+
+<template>
+  <div class="container">
+    <h1>VCheckbox</h1>
+    <div class="section_body">
+      <p>Пример компонента VCheckbox</p>
+      <vcheckbox :idCheckBox="idCheckBox" labelText="Hello world"></vcheckbox>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10px;
+}
+
+h1::first-letter {
+  color: rgb(61, 228, 61);
+}
+
+h1 {
+  color: rgb(54, 54, 54);
+}
+
+.section_body {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/VCheckbox.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="v_checkbox_container">
+    <input
+      type="checkbox"
+      :checked="isChecked"
+      :name="nameCheckBox"
+      :id="idCheckBox"
+      :value="value"
+      :disabled="disabled"
+      :required="required"
+      @change="changeChecked"
+    />
+    <slot name="v_label"
+      ><label :for="idCheckBox">{{ labelText }}</label></slot
+    >
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  isChecked: {
+    type: Boolean,
+    default: false,
+  },
+  idCheckBox: {
+    type: String,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+  nameCheckBox: String,
+  labelText: {
+    type: String,
+    default: "",
+  },
+});
+
+const emits = defineEmits(["update:checked"]);
+
+const changeChecked = (event) => {
+  emits("update:checked", event.target.checked);
+};
+</script>
+
+<style scoped>
+.v_checkbox_container input[type="checkbox"] {
+  display: none;
+}
+
+:slotted(label) {
+  display: flex;
+  margin-right: 5px;
+  justify-content: center;
+  align-items: center;
+}
+
+:slotted(label)::before {
+  content: "";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #e3e3;
+  border-radius: 50%;
+  margin-right: 5px;
+  transition: 0.2s;
+}
+
+:slotted(label):hover::before {
+  cursor: pointer;
+}
+
+input[type="checkbox"]:checked + :slotted(label)::before {
+  background-color: red;
+  content: "V";
+  color: #fff;
+}
+</style>
